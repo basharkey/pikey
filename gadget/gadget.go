103dde65214fa5843d9bf9edf6b64b73856a823d
15859442+basharkey@users.noreply.github.com <15859442+basharkey@users.noreply.github.com>
2022-04-24 18:02:27 +0100

    document report desc, gadget destroy func

--- gadget/gadget.go
+++ gadget/gadget.go
@@ -5,43 +5,128 @@ import (
     //"log"
     "fmt"
     "path/filepath"
+    "os/exec"
 )
 
+var base_dir = "/sys/kernel/config/usb_gadget/pimk"
+
+var usb_string = "0x409"
+var usb_config = "c.1"
+var usb_device = "hid.usb0"
+
+var strings_dir = filepath.Join("strings", usb_string)
+var configs_dir = filepath.Join("configs", usb_config)
+var functions_dir = filepath.Join("functions", usb_device)
+
 func Initialize() {
-    var base_path string = "/sys/kernel/config/usb_gadget/pimk"
 
     var files = [][]string {
-        {"idVendor", "0x1d6b"}, // Linux Foundation
+        {"idVendor", "0xbeaf"}, // Linux Foundation
         {"idProduct", "0x0104"}, // Multifunction Composite Gadget
         {"bcdDevice", "0x0100"}, // v1.0.0
         {"bcdUSB", "0x0200"}, // USB2
 
-        {"strings/0x409/serialnumber", "fedcba9876543210"},
-        {"strings/0x409/manufacturer", "Pimk"},
-        {"strings/0x409/product", "Pimk USB Keyboard Device"},
+        {filepath.Join(strings_dir, "serialnumber"), "fedcba9876543210"},
+        {filepath.Join(strings_dir, "manufacturer"), "Pimk"},
+        {filepath.Join(strings_dir, "product"), "USB Keyboard Device"},
 
-        {"configs/c.1/strings/0x409/configuration", "Config 1: ECM network"},
-        {"configs/c.1/MaxPower", "250"},
+        {filepath.Join(configs_dir, strings_dir, "configuration"), "Config 1: ECM network"},
+        {filepath.Join(configs_dir, "MaxPower"), "250"},
 
-        {"functions/hid.usb0/protocol", "1"},
-        {"functions/hid.usb0/subclass", "1"},
-        {"functions/hid.usb0/report_length", "8"},
-        {"functions/hid.usb0/report_desc", "\x05\x01\x09\x06\xa1\x01\x85\x01\x05\x07\x19\xe0\x29\xe7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x03\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x03\x95\x06\x75\x08\x15\x00\x25\x73\x05\x07\x19\x00\x29\x73\x81\x00\xc0\x05\x0c\x09\x01\xa1\x01\x85\x02\x05\x0c\x15\x00\x25\x01\x75\x01\x95\x07\x09\xb5\x09\xb6\x09\xb7\x09\xcd\x09\xe2\x09\xe9\x09\xea\x81\x02\x95\x01\x81\x01\xc0"},
+        {filepath.Join(functions_dir, "protocol"), "1"},
+        {filepath.Join(functions_dir, "subclass"), "1"},
+        {filepath.Join(functions_dir, "report_length"), "8"},
+        // sudo usbhid-dump -d beaf | tail -n +2 | xxd -r -p | hidrd-convert -o spec
+        {filepath.Join(functions_dir, "report_desc"),
+            "\x05\x01" + // Usage Page (Desktop)
+            "\x09\x06" + // Usage (Keyboard)
+            "\xa1\x01" + // Collection (Application)
+            "\x85\x01" + // Report ID (1)
+            "\x05\x07" + // Usage Page (Keyboard)
+            "\x19\xe0" + // Usage Minimum (KB Leftcontrol)
+            "\x29\xe7" + // Usage Maximum (KB Right GUI)
+            "\x15\x00" + // Logical Minimum (0)
+            "\x25\x01" + // Logical Maximum (1)
+            "\x75\x01" + // Report Size (1)
+            "\x95\x08" + // Report Count (8)
+            "\x81\x02" + // Input (Variable)
+            "\x95\x01" + // Report Count (1)
+            "\x75\x08" + // Report Size (8)
+            "\x81\x03" + // Input (Constant, Variable)
+            "\x95\x05" + // Report Count (5)
+            "\x75\x01" + // Report Size (1)
+            "\x05\x08" + // Usage Page (LED)
+            "\x19\x01" + // Usage Minimum (01h)
+            "\x29\x05" + // Usage Maximum (05h)
+            "\x91\x02" + // Output (Variable)
+            "\x95\x01" + // Report Count (1)
+            "\x75\x03" + // Report Size (3)
+            "\x91\x03" + // Output (Constant, Variable)
+            "\x95\x06" + // Report Count (6)
+            "\x75\x08" + // Report Size (8)
+            "\x15\x00" + // Logical Minimum (0)
+            "\x25\x73" + // Logical Maximum (115)
+            "\x05\x07" + // Usage Page (Keyboard)
+            "\x19\x00" + // Usage Minimum (None)
+            "\x29\x73" + // Usage Maximum (KB F24)
+            "\x81\x00" + // Input
+            "\xc0" + // End Collection
+            "\x05\x0c" + // Usage Page (Consumer)
+            "\x09\x01" + // Usage (Consumer Control)
+            "\xa1\x01" + // Collection (Application)
+            "\x85\x02" + // Report ID (2)
+            "\x05\x0c" + // Usage Page (Consumer)
+            "\x15\x00" + // Logical Minimum (0)
+            "\x25\x01" + // Logical Maximum (1)
+            "\x75\x01" + // Report Size (1)
+            "\x95\x14" + // Report Count (7)
+            "\x09\xb5" + //
+            "\x09\xb6" + //
+            "\x09\xb7" + //
+            "\x09\xcd" + //
+            "\x09\xe2" + //
+            "\x09\xe9" + //
+            "\x09\xea" + //
+            "\x81\x02" + // Input (Variable)
+            "\x95\x01" + // Report Count (1)
+            "\x81\x01" + // Input (Constant)
+            "\xc0"}, // End Collection
     }
 
     for _, file := range files {
-        write_to_file(filepath.Join(base_path, file[0]), file[1])
+        write_to_file(filepath.Join(base_dir, file[0]), file[1])
     }
 
-    link := filepath.Join(base_path, "functions/hid.usb0")
-    target := filepath.Join(base_path, "configs/c.1/hid.usb0")
+    link := filepath.Join(base_dir, functions_dir)
+    target := filepath.Join(base_dir, filepath.Join(configs_dir, usb_device))
     os.Symlink(link, target)
 
     dir, err := os.Open("/sys/class/udc/")
     check_err(err)
     file, err := dir.Readdir(0)
     check_err(err)
-    write_to_file(filepath.Join(base_path, "UDC"), file[0].Name())
+    write_to_file(filepath.Join(base_dir, "UDC"), file[0].Name())
+}
+
+func Destroy() {
+    var base_dir string = "/sys/kernel/config/usb_gadget/pimk"
+    // clear UDC file data, don't think there is a way to do this with pure go
+    //https://askubuntu.com/questions/823380/cannot-delete-residual-system-files-period-even-after-changing-permissions-as-r
+    cmd := exec.Command("/usr/bin/env", "bash", "-c", "echo '' > " + filepath.Join(base_dir, "UDC"))
+    cmd.Run()
+
+    // remove strings from configs
+    os.Remove(filepath.Join(base_dir, configs_dir, strings_dir))
+    // remove functions from configs
+    os.Remove(filepath.Join(base_dir, configs_dir, usb_device))
+    // remove configs
+    os.Remove(filepath.Join(base_dir, configs_dir))
+    // remove functions
+    os.Remove(filepath.Join(base_dir, functions_dir))
+    // remove strings
+    os.Remove(filepath.Join(base_dir, strings_dir))
+    // remove gadget
+    os.Remove(base_dir)
 }
 
 func check_err(err error) {
@@ -55,7 +140,7 @@ func write_to_file(file_path string, file_content string) {
     file_dir := filepath.Dir(file_path)
     _, err := os.Stat(file_dir)
     if os.IsNotExist(err) {
-        err := os.MkdirAll(file_dir, 0755)
+        err := os.MkdirAll(file_dir, 0644)
         check_err(err)
     }
 
